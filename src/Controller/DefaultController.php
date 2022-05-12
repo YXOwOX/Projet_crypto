73a0da4854259f5d23cd81e4c45ab94fd0a2e129
YXOwOX <thibault-lb@hotmail.fr>
2022-05-12 19:27:50 +0200

    Redirection automatique vers le login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(UrlGeneratorInterface $urlGenerator): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+
+        return new RedirectResponse($urlGenerator->generate('app_login'));
+
+        // return $this->render('default/index.html.twig', [
+        //     'controller_name' => 'DefaultController',
+        // ]);
     }
 }
